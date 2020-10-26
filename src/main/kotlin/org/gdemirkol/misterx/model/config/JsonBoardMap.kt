95d9d23c4302561ba341002362b9696709b40113
Fuchs, Stefan <stefan.fuchs@willhaben.at>
2020-10-26 17:20:56 +0100

    added webserver

--- src/main/kotlin/org/gdemirkol/misterx/model/config/JsonBoardMap.kt
+++ src/main/kotlin/org/gdemirkol/misterx/model/config/JsonBoardMap.kt
@@ -3,8 +3,10 @@ package org.gdemirkol.misterx.model.config
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class JsonBoardMap(val connections: List<JsonConnection>)
+data class JsonBoardMap(val stations: List<JsonStation>, val connections: List<JsonConnection>)
 
+@Serializable
+data class JsonStation(val stationId: Int, val locationX: Int, val locationY: Int)
 
 @Serializable
 data class JsonConnection(val sourceStationId: Int, val transportationType: String, val targetStationId: Int)
